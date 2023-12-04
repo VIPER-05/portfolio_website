2ca2d193fbfeafda6d4b8e34df740df38f428e2f
Vaibhav Agria <147040710+VIPER-05@users.noreply.github.com>
2023-12-04 13:52:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+ 
+ 
+ function redirectToLinkedln() {
+     window.location.href = 'https://www.linkedin.com/in/vaibhav-agria-407a6324a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app';  
+ }
+ 
+ 
+var phoneNumber = '9773642825';
+ 
+ 
+var message = 'Hello!  want to get in touch.';
+
+ 
+var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(message);
+
+ 
+ 
+ 
+function redirectToWhatsapp() {
+    window.location.href = whatsappLink;
+}
+
+ 
+ 
+ 
+ 
+ 
